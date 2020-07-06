26cc6ef5d6de0b8214998d8a1cb5d5546eb2080e
N-P-Williams <44409336+N-P-Williams@users.noreply.github.com>
2020-07-06 16:15:32 -0600

    Add files via upload

---
+++ Twiiter_API.py
@@ -0,0 +1,48 @@
+import tweepy
+import GetOldTweets3 as got
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Login to twitter
+# auth = tweepy.OAuthHandler(api_key, api_secret_key)
+# auth.set_access_token(access_token, access_secret)
+# api = tweepy.API(auth)
+
+
+def get_tweets(search_word, start_date, end_date, max_tweets):
+    """
+    :param search_word: word to be searched
+    :param start_date: date to begin search YYYY-MM-DD
+    :param end_date: date to end search YYYY-MM-DD
+    :param max_tweets: maximum number of tweets to return
+     NW: anything over 10,000 tweets took to long to return for me, also i tend to keep the date range close to a week
+    to not miss out on any tweets.
+    """
+    tweetCriteria = got.manager.TweetCriteria().setQuerySearch(search_word) \
+        .setSince(start_date) \
+        .setUntil(end_date) \
+        .setNear('Utah') \
+        .setWithin('500mi') \
+        .setMaxTweets(max_tweets)
+    tweets = got.manager.TweetManager.getTweets(tweetCriteria)
+
+    # put all tweet info in a list of lists
+
+    text_tweets = [[tw.username,
+                    tw.text,
+                    tw.date,
+                    tw.retweets,
+                    tw.favorites,
+                    tw.mentions,
+                    tw.hashtags] for tw in tweets]
+    # add all info into a pandas dataframe
+    news_df = pd.DataFrame(text_tweets,
+                           columns=['User', 'Text', 'Date', 'Favorites', 'Retweets', 'Mentions', 'HashTags'])
+    return news_df
+
+
+if __name__ == "__main__":
+    tweet_df = get_tweets(search_word='corona', start_date='2020-01-01', end_date='2020-01-08', max_tweets=100)
+    # exporting as an html file
+    tweet_df.to_html('twitter_summary.html')
+
