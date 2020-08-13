59b9c959d4652955730c81a893b43310e9fd5dea
N-P-Williams <44409336+N-P-Williams@users.noreply.github.com>
2020-08-13 14:10:15 -0600

    Add files via upload

---
+++ multipleLinearRegression.py
@@ -0,0 +1,98 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as seabornInstance
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+
+
+def read_csv_to_df():
+    df = pd.read_csv('Clean_Data/df_data.csv').drop(['Unnamed: 0'], axis=1)
+    return df
+
+
+# 
+def mask_linear_regression():
+    dataset = read_csv_to_df()
+
+    X = dataset['Mask'].values.reshape(-1, 1)
+    y = dataset['Cases'].values.reshape(-1, 1)
+    # 80% training 20% testing split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+    regressor = LinearRegression()
+    regressor.fit(X_train, y_train)
+
+    # To retrieve the intercept:
+    print(regressor.intercept_)
+    # For retrieving the slope:
+    print(regressor.coef_)
+
+    # make some predictions
+    y_pred = regressor.predict(X_test)
+    df = pd.DataFrame({'Actual': y_test.flatten(), 'Predicted': y_pred.flatten()})
+    #print(df)
+    # visualize predictions vs actual
+    #df1 = df
+    #df1.plot(kind='bar', figsize=(16, 10))
+    #plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
+    #plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+    #plt.show()
+
+    # show predicted line over actual data
+    plt.scatter(X_test, y_test, color='gray')
+    plt.plot(X_test, y_pred, color='red', linewidth=2)
+    plt.show()
+
+    # evaluate preformance
+    print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+    print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
+    print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
+
+
+def do_some_regression():
+    dataset = read_csv_to_df()
+
+    # plt.figure(figsize=(15, 10))
+    # plt.tight_layout()
+    # seabornInstance.distplot(dataset['Cases'])
+    # plt.show()
+    print(dataset['Cases'])
+
+    X = dataset[['Corona', 'Covid', 'Flu', 'Hand Sanitizer', 'Mask', 'Virus']]
+    y = dataset['Cases']
+    # Split data into 80% training 20% testing
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+    regressor = LinearRegression()
+    regressor.fit(X_train, y_train)
+
+    # check coefficients
+    coeff_df = pd.DataFrame(regressor.coef_, X.columns, columns=['Coefficient'])
+    # print(coeff_df)
+
+    # do predictions on test data
+    y_pred = regressor.predict(X_test)
+
+    # actual value vs predicted value
+    df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
+    df1 = df.head(25)
+    # print(df1)
+
+    # Plot actual vs predicted
+    df1.plot(kind='bar', figsize=(10, 8))
+    plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
+    plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+    # plt.show()
+
+    # Check preformance of algorithm
+    print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+    print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
+    print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
+
+if __name__ == '__main__':
+    #do_some_regression()
+    mask_linear_regression()
